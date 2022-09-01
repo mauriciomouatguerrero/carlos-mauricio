12d3f82620cae748ed967566f7b2d99cb927c288
mauriciomouatguerrero <mauricio.mouat.guerrero@gmail.com>
2022-09-01 17:45:27 -0400

    AJAX-JSON (Cliente/Server)
    implementación AJAX/JSON (Transporte y llegada de datos - Cliente/Servidor)

---
+++ tests/getmydata.php
@@ -0,0 +1,9 @@
+<?php
+   //print "Welcome to getmydata... (PHP 7.4)";
+   $array = array(
+      'nombre' => "mauricio",
+      'telefono' => "+56965474096"
+   );
+
+   print json_encode($array);
+?>
