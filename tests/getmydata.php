95c3374ae0c3d5c814758d211b311a214af2e51f
mauriciomouatguerrero <mauricio.mouat.guerrero@gmail.com>
2022-09-01 18:20:23 -0400

    passing data AJAX/JSON/PHP

--- tests/getmydata.php
+++ tests/getmydata.php
@@ -1,9 +1,14 @@
 <?php
    //print "Welcome to getmydata... (PHP 7.4)";
-   $array = array(
-      'nombre' => "mauricio",
-      'telefono' => "+56965474096"
+   //$array = array(
+   //   'nombre' => "mauricio",
+   //   'telefono' => "+56965474096"
+   //);
+   //print json_encode($array);
+
+   $array_input = array(
+   	   "cadena" => $_POST['cadena']
    );
 
-   print json_encode($array);
+   print json_encode($array_input);
 ?>
